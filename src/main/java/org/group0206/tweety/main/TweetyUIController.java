0cd38414e1582d904b53c7cd39e1b0293e4e1f5b
michael <msb0094@auburn.edu>
2021-11-16 22:07:42 -0600

    fixed scheduler logic

--- src/main/java/org/group0206/tweety/main/TweetyUIController.java
+++ src/main/java/org/group0206/tweety/main/TweetyUIController.java
@@ -14,11 +14,8 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class TweetyUIController extends AnchorPane {
 
